15db51936b9c2afe0ead5cae73d3d73f7265b505
Jaekwon_Kim <jaekwon.kim@ybrain.com>
2024-03-18 17:14:54 +0900

    Template Queue 완료.

--- TemplateQueue.h
+++ TemplateQueue.h
@@ -28,13 +28,8 @@ public:
     uint16_t push(T data) {
         uint16_t return_val = 0;
 
-        if (isFull() == true) {
-            inc_front();
-        }
-
         Q[get_end()] = data;
-        inc_end();
-        return_val = 1;
+        return_val = inc_end();
 
         return return_val;
     }
@@ -43,6 +38,8 @@ public:
         uint16_t return_val = 0;
         uint16_t push_left_size = 0;
         uint16_t end_pos_left_size = 0;
+        uint16_t backup_front = get_front();
+        uint16_t backup_end = get_end();
 
         end_pos_left_size = get_delta_end(get_end());
 
@@ -61,6 +58,8 @@ public:
             set_end(get_end() + push_size);
         }
 
+        set_front(check_inc_edge(cal_pos(backup_front, backup_end, push_size)));
+
         return return_val;
     }
 
@@ -68,8 +67,7 @@ public:
         uint16_t return_val = 0;
 
         if (isEmpty() == false) {
-            inc_front();
-            return_val = 1;
+            return_val = inc_front();
         }
 
         return return_val;
@@ -81,24 +79,26 @@ public:
         uint16_t pop_left_size = 0;
         uint16_t end_pos_left_size = 0;
 
-        data_left_size = get_Q_size();
+        if (isEmpty() == false) {
+            data_left_size = get_Q_size();
 
-        if (pop_size < data_left_size) {
-            end_pos_left_size = get_delta_end(get_front());
+            if (pop_size < data_left_size) {
+                end_pos_left_size = get_delta_end(get_front());
 
-            if (end_pos_left_size < pop_size) {
-                return_val += end_pos_left_size;
+                if (end_pos_left_size < pop_size) {
+                    return_val += end_pos_left_size;
 
-                pop_left_size = pop_size - end_pos_left_size;
-                return_val += pop_left_size;
+                    pop_left_size = pop_size - end_pos_left_size;
+                    return_val += pop_left_size;
 
-                set_front(pop_left_size);
+                    set_front(pop_left_size);
+                } else {
+                    return_val += pop_size;
+                    set_front(get_front() + pop_size);
+                }
             } else {
-                return_val += pop_size;
-                set_front(get_front() + pop_size);
+                return_val = popAll();
             }
-        } else {
-            return_val = popAll();
         }
 
         return return_val;
@@ -107,8 +107,10 @@ public:
     uint16_t popAll() {
         uint16_t return_val = 0;
 
-        return_val = get_Q_size();
-        set_front(get_end());
+        if (isEmpty() == false) {
+            return_val = get_Q_size();
+            set_front(get_end());
+        }
 
         return return_val;
     }
@@ -129,41 +131,55 @@ public:
             end_pos_left_size = get_delta_end(get_front());
 
             if (end_pos_left_size < read_size) {
-                memcpy(&read_buf[0], Q[get_front()], end_pos_left_size);
+                memcpy(&read_buf[0], &Q[get_front()], end_pos_left_size);
                 return_val += end_pos_left_size;
 
                 read_left_size = read_size - end_pos_left_size;
-                memcpy(&read_buf[end_pos_left_size], Q[0], read_left_size);
+                memcpy(&read_buf[end_pos_left_size], &Q[0], read_left_size);
                 return_val += read_left_size;
             } else {
-                memcpy(&read_buf[0], Q[get_front()], read_size);
+                memcpy(&read_buf[0], &Q[get_front()], read_size);
                 return_val += read_size;
             }
         } else {
-            return_val = readAll(read_buf);
+            return_val = readAll(read_buf, read_size);
         }
 
         return return_val;
     }
 
-    uint16_t readAll(T *read_buf) {
+    uint16_t readAll(T *read_buf, uint16_t read_buf_size) {
         uint16_t return_val = 0;
-        uint16_t data_left_size = 0;
+        uint16_t data_left_size = get_Q_size();
         uint16_t read_left_size = 0;
         uint16_t end_pos_left_size = 0;
 
         end_pos_left_size = get_delta_end(get_front());
 
-        if (end_pos_left_size < data_left_size) {
-            memcpy(&read_buf[0], Q[get_front()], end_pos_left_size);
-            return_val += end_pos_left_size;
+        if (data_left_size > read_buf_size) {
+            if (end_pos_left_size < read_buf_size) {
+                memcpy(&read_buf[0], &Q[get_front()], end_pos_left_size);
+                return_val += end_pos_left_size;
 
-            read_left_size = data_left_size - end_pos_left_size;
-            memcpy(&read_buf[end_pos_left_size], Q[0], read_left_size);
-            return_val += read_left_size;
+                read_left_size = read_buf_size - end_pos_left_size;
+                memcpy(&read_buf[end_pos_left_size], &Q[0], read_left_size);
+                return_val += read_left_size;
+            } else {
+                memcpy(&read_buf[0], &Q[get_front()], read_buf_size);
+                return_val += read_buf_size;
+            }
         } else {
-            memcpy(&read_buf[0], Q[get_front()], data_left_size);
-            return_val += data_left_size;
+            if (end_pos_left_size < data_left_size) {
+                memcpy(&read_buf[0], &Q[get_front()], end_pos_left_size);
+                return_val += end_pos_left_size;
+
+                read_left_size = data_left_size - end_pos_left_size;
+                memcpy(&read_buf[end_pos_left_size], &Q[0], read_left_size);
+                return_val += read_left_size;
+            } else {
+                memcpy(&read_buf[0], &Q[get_front()], data_left_size);
+                return_val += data_left_size;
+            }
         }
 
         return return_val;
@@ -246,22 +262,86 @@ private:
         end = check_inc_edge(temp_end);
     }
 
-    void inc_front() {
-        set_front(inc_pos(get_front()));
+    uint16_t inc_front() {
+        uint16_t return_val = 0;
+
+        if (isEmpty() == false) {
+            set_front(inc_pos(get_front()));
+            return_val = 1;
+        }
+
+        return return_val;
     }
 
-    void inc_end() {
+    uint16_t inc_front(uint16_t temp_size) {
+        uint16_t return_val = 0;
+        uint16_t end_pos_left_size = 0;
+
+        end_pos_left_size = get_delta_end(get_front());
+
+        if (end_pos_left_size < temp_size) {
+            set_front(temp_size - end_pos_left_size);
+        } else {
+            set_front(get_front() + temp_size);
+        }
+
+        return return_val;
+    }
+
+    uint16_t inc_end() {
+        uint16_t return_val = 0;
+
+        if (isFull() == true) {
+            inc_front();
+        }
+
         set_end(inc_pos(get_end()));
+        return_val = 1;
+
+        return return_val;
+    }
+
+    uint16_t inc_end(uint16_t temp_size) {
+        uint16_t return_val = 0;
+        uint16_t end_pos_left_size = 0;
+
+        end_pos_left_size = get_delta_end(get_end());
+
+        if (end_pos_left_size < temp_size) {
+            set_end(temp_size - end_pos_left_size);
+        } else {
+            set_end(get_end() + temp_size);
+        }
+
+        return return_val;
     }
 
     uint16_t get_left_space() {
         return getMaxSize() - get_Q_size() - 1;
     }
 
+    uint16_t get_left_space(uint16_t temp_front, uint16_t temp_end) {
+        return getMaxSize() - get_Q_size(temp_front, temp_end) - 1;
+    }
+
     uint16_t get_Q_size() {
         uint16_t return_val = 0;
         uint16_t temp_front = get_front();
         uint16_t temp_end = get_end();
+
+        if (temp_front == temp_end) {
+            return_val = 0;
+        } else if (temp_front < temp_end) {
+            return_val = temp_end - temp_front;
+        } else {
+            return_val = (temp_end + getMaxSize()) - temp_front;
+        }
+
+        return return_val;
+    }
+
+    uint16_t get_Q_size(uint16_t temp_front, uint16_t temp_end) {
+        uint16_t return_val = 0;
 
         if (temp_front == temp_end) {
             return_val = 0;
@@ -310,6 +390,29 @@ private:
         }
 
         return_val = static_cast<uint16_t>(temp_pos);
+
+        return return_val;
+    }
+
+    uint16_t cal_pos(uint16_t temp_front, uint16_t temp_end,
+            uint16_t temp_size) {
+        uint16_t return_val = 0;
+        uint16_t available_push_size = get_left_space(temp_front, temp_end);
+        uint16_t pop_by_full_size = 0;
+        uint16_t front_pos_left_size = 0;
+
+        if (available_push_size < temp_size) {
+            pop_by_full_size = temp_size - available_push_size;
+            front_pos_left_size = get_delta_end(temp_front);
+        }
+
+        if (pop_by_full_size > 0) {
+            if (front_pos_left_size < pop_by_full_size) {
+                return_val = pop_by_full_size - front_pos_left_size;
+            } else {
+                return_val = temp_front + pop_by_full_size;
+            }
+        }
 
         return return_val;
     }
